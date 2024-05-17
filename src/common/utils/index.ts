c218c1666b736b3c9c85f742c41084062c037fd7
dev <dev@gmail.com>
2024-05-18 03:35:53 +0800

    utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -6,6 +6,51 @@ export function page(pageIndex: number, pageSize: number, where: any) {
     };
 }
 
+export function mapToObj(map: any) {
+    return Array.from(map).reduce((obj, [key, value]) => {
+        obj[key] = value;
+        return obj;
+    }, {});
+}
+
+export function sleep(seconds: number) {
+    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+export function random(min: number, max: number) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+export function shuffle(arr: any) {
+    let _arr = arr.slice();
+    for (let i = 0; i < _arr.length; i++) {
+        let temp = _arr[i];
+        let j = this.random(0, i);
+        _arr[i] = _arr[j];
+        _arr[j] = temp;
+    }
+    return _arr;
+}
+
+export function randomMap(start: any, end: any, arr: any) {
+    let _start = arr.length + start;
+    let map: any = {};
+    for (let i = _start; i < end; i++) {
+        map[i] = i;
+    }
+
+    let index = end - 1;
+    while (index >= start) {
+        let num = this.random(start, index);
+        let temp = map[num];
+        map[num] = map[index];
+        map[index] = temp;
+        index--;
+    }
+
+    return map;
+}
+
 export function cloneDeep(data: any) {
     return JSON.parse(JSON.stringify(data));
 }
